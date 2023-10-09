87f982617423bd8479ef3314e933d5b2db3e5ca5
alexcalister <alister707@gmail.com>
2023-10-09 20:56:01 +0300

    Feat: add yandex-metrika

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-
 import { Vue3Mq } from "vue3-mq"
-
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { initYandexMetrika } from 'yandex-metrika-vue3';
+
+import App from './App.vue'
 
 const app = createApp(App)
 app.use(Vue3Mq, {
   preset: 'wordpress'
-})
-app.use(VueAxios, axios)
-app.mount('#app')
+});
+app.use(VueAxios, axios);
+app.use(initYandexMetrika, {
+  id: 95199342,
+  options: {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true,
+  },
+});
+app.mount('#app');
