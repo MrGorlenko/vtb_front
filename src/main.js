8bdf981038a3fc9931df370dfebe5331f51cf66b
alexcalister <alister707@gmail.com>
2023-10-10 21:38:55 +0300

    Feat: add vue-router, fix yandex-metrika

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from "vue-router";
 import { Vue3Mq } from "vue3-mq"
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -6,13 +7,23 @@ import { initYandexMetrika } from 'yandex-metrika-vue3';
 
 import App from './App.vue'
 
+const routes = [
+  { path: '/', component: () => import('./App.vue') },
+];
+const router = createRouter({
+  routes,
+  history: createWebHistory(),
+});
+
 const app = createApp(App)
 app.use(Vue3Mq, {
   preset: 'wordpress'
 });
 app.use(VueAxios, axios);
 app.use(initYandexMetrika, {
+  router,
   id: 95199342,
+  env: process.env.NODE_ENV || 'production',
   options: {
     clickmap: true,
     trackLinks: true,
